413a580e3a8d10f09d6593db451000541ede6d17
jaeheon jung <>
2021-10-18 18:48:30 +0900

    init source

---
+++ components/IconLeft.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+function SvgComponent(props) {
+  return (
+    <Svg viewBox="0 0 24 24" {...props}>
+      <Path
+        fill={props.color}
+        d="M8.5 12.8l5.7 5.6c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4l-4.9-5 4.9-5c.4-.4.4-1 0-1.4-.2-.2-.4-.3-.7-.3-.3 0-.5.1-.7.3l-5.7 5.6c-.4.5-.4 1.1 0 1.6 0-.1 0-.1 0 0z"
+      />
+    </Svg>
+  )
+}
+
+export default SvgComponent
